0c90a2df7b6962c1cd428d83334d56787e8d4b19
RosanneE <anderson.rosanne4@gmail.com>
2022-11-17 17:12:14 -0500

    Tictac board creation

--- src/pages/Tictac.js
+++ src/pages/Tictac.js
@@ -2,6 +2,21 @@ const Main = () => {
     return (
         <div>
             <h2>TicTacToe</h2>
+            <div className="row">
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            </div>
+            <div lassName="row">
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            </div>
+            <div lassName="row">
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            <button className="ticTacSqu"></button>
+            </div>
         </div>
         
     )
