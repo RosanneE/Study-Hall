70c9b4cbdba355c1823e726b97bf8bee418dc355
RosanneE <anderson.rosanne4@gmail.com>
2022-11-18 21:49:54 -0500

    boolean turn change on click

--- src/pages/Tictac.js
+++ src/pages/Tictac.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 const Main = () => {
   let inHtml = "";
-
   let turn = true
+  let xIm = 'red'
+//   'https://constablesestateagents.co.uk/wp-content/uploads/2020/11/185-1857130_grunge-x-png-transparent-png.png'
+  let oIm = 'blue'
+//   'https://www.kindpng.com/picc/m/33-339417_black-outline-circle-png-transparent-png.png'
+  
+  const [isTurn, setIsTurn] = useState(false)
+
+  const changeTurn = () => {
+    setIsTurn(current => !current)
+  }
 
   const setSqu = (sq) => {
-    
-    // if (inHtml === "" && turn=== true) {
-    //     console.log(this)
-    // }
+    setIsTurn(current => !current)
+    console.log(turn)
   };
 
   return (
@@ -15,7 +24,7 @@ const Main = () => {
       <h2>TicTacToe</h2>
       <div className = "ticBoard">
         <div className="row">
-          <button className="ticTacSqu" style={{backgroundColor: 'red'}}></button>
+          <button className="ticTacSqu" onClick={setSqu} style={{backgroundColor: isTurn ?  '{{xIm}}' : '{{oIm}}', backgroundSize: 'cover'}}></button>
           <div className="ticTacSqu"></div>
           <div className="ticTacSqu"></div>
         </div>
