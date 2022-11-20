0cc36ae52dece012e3c1714a40e179fd4c365fe6
RosanneE <anderson.rosanne4@gmail.com>
2022-11-19 22:10:00 -0500

    format quares as buttons, color update

--- src/pages/Tictac.js
+++ src/pages/Tictac.js
@@ -2,41 +2,100 @@ import { useState } from "react";
 
 const Main = () => {
   let inHtml = "";
-  let turn = true
-  let xIm = 'red'
-//   'https://constablesestateagents.co.uk/wp-content/uploads/2020/11/185-1857130_grunge-x-png-transparent-png.png'
-  let oIm = 'blue'
-//   'https://www.kindpng.com/picc/m/33-339417_black-outline-circle-png-transparent-png.png'
-  
-  const [isTurn, setIsTurn] = useState(false)
+  let turn = true;
+  let xIm = "red";
+  //   'https://constablesestateagents.co.uk/wp-content/uploads/2020/11/185-1857130_grunge-x-png-transparent-png.png'
+  let oIm = "purple";
+  //   'https://www.kindpng.com/picc/m/33-339417_black-outline-circle-png-transparent-png.png'
 
-  const changeTurn = () => {
-    setIsTurn(current => !current)
-  }
+  const [isTurn, setIsTurn] = useState(false);
 
   const setSqu = (sq) => {
-    setIsTurn(current => !current)
-    console.log(isTurn)
+    setIsTurn((current) => !current);
+    console.log(isTurn);
   };
 
   return (
     <div>
       <h2>TicTacToe</h2>
-      <div className = "ticBoard">
+      <div className="ticBoard">
         <div className="row">
-          <button className="ticTacSqu" onClick={setSqu} style={{backgroundColor: isTurn ?  '{{xIm}}' : '{{oIm}}', backgroundSize: 'cover'}}></button>
-          <div className="ticTacSqu"></div>
-          <div className="ticTacSqu"></div>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
         </div>
         <div className="row">
-          <div className="ticTacSqu"></div>
-          <div className="ticTacSqu"></div>
-          <div className="ticTacSqu"></div>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
         </div>
         <div className="row">
-          <div className="ticTacSqu"></div>
-          <div className="ticTacSqu"></div>
-          <div className="ticTacSqu"></div>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
+          <button
+            className="ticTacSqu"
+            onClick={setSqu}
+            style={{
+              backgroundColor: isTurn ? "red" : "purple",
+              backgroundSize: "cover",
+            }}
+          ></button>
         </div>
       </div>
     </div>
