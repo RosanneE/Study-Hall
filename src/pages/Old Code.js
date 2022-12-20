02437e6021776f1931d9af833645a56e1a957fe9
RosanneE <anderson.rosanne4@gmail.com>
2022-12-19 20:54:25 -0500

    row 1 tictac testing

--- src/pages/Old Code.js
+++ src/pages/Old Code.js
@@ -127,24 +127,24 @@
 //       const status = "Player X is up!";
 
 //       return (
-//         <div>
-//           <div className="ticStatus">{status}</div>
-//           <div className="tic-row">
-//             {this.renderSquare(0)}
-//             {this.renderSquare(1)}
-//             {this.renderSquare(2)}
-//           </div>
-//           <div className="tic-row">
-//             {this.renderSquare(3)}
-//             {this.renderSquare(4)}
-//             {this.renderSquare(5)}
-//           </div>
-//           <div className="tic-row">
-//             {this.renderSquare(6)}
-//             {this.renderSquare(7)}
-//             {this.renderSquare(8)}
-//           </div>
-//         </div>
+        <div>
+          <div className="ticStatus">{status}</div>
+          <div className="tic-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="tic-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="tic-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
 //       );
 //     }
 //   }
--- src/pages/Tictac.js
+++ src/pages/Tictac.js
@@ -1,6 +1,26 @@
-import React, { components } from "react";
+import React, { useState } from "react";
 
 const TicTac = () => {
+  const[inPlay, setInPlay] = useState(false)
 
+
+
+  const styleInPlay = () =>{
+    setInPlay ? setInPlay = true : setInPlay = false
+    console.log(inPlay )
+  }
+  
+  return (
+    <div>
+      <h1>TicTacToe</h1>
+      <div>
+        <div className="tic-row">
+          <button className="ticSquare" style={{color: inPlay? 'black' : 'light grey'}} onClick={styleInPlay}> XO</button>
+          <button className="ticSquare"> XO </button>
+          <button className="ticSquare"> XO </button>
+        </div>
+      </div>
+    </div>
+  );
 };
- export default TicTac
+export default TicTac;
